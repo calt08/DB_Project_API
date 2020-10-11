6dd2a6abb68fec99bd7363db07199a43cb0766f5
calt08 <cesarlopez2808@gmail.com>
2020-10-11 11:35:45 -0400

    Added functions to build endpoint

--- SQL_Scripts/select_functions.sql
+++ SQL_Scripts/select_functions.sql
@@ -4,5 +4,3 @@ AS $$
 SELECT * FROM CUSTOMER WHERE email = reqEmail;
 $$
 language sql;
-
-SELECT * FROM Get_User('cesar@lopez.com')
--- src/utils/Setup.route.ts
+++ src/utils/Setup.route.ts
@@ -8,10 +8,12 @@ router.get('/build', async (req: Request, res: Response): Promise<Response> => {
     try {
         const TablesQueries = fs.readFileSync('SQL_Scripts/create_tables.sql').toString();
         const DMLQueries = fs.readFileSync('SQL_Scripts/DML_procedures.sql').toString();
+        const SelectQueries = fs.readFileSync('SQL_Scripts/select_functions.sql').toString();
         const tables = await pool.query(TablesQueries);
         const SP = await pool.query(DMLQueries);
+        const selects = await pool.query(SelectQueries);
 
-        return res.status(200).send({ msg: 'Tables and Stored Procedures created', tables, SP });
+        return res.status(200).send({ msg: 'Tables, Functions and Stored Procedures created', tables, SP, selects });
 
     } catch (error) {
         return res.status(500).send({ msg: 'Something Unexpected happened', error })
